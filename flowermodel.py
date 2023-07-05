6a303191f2cc75446915bf66af4d8fbc4c5c8582
poojithaf1 <138689447+poojithaf1@users.noreply.github.com>
2023-07-05 19:24:50 +0530

    Add files via upload

---
+++ flowermodel.py
@@ -0,0 +1,28 @@
+# Import necessary libraries
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+# Load the Iris dataset
+iris = load_iris()
+
+# Split the dataset into features (X) and target (y)
+X = iris.data
+y = iris.target
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a K-Nearest Neighbors classifier
+knn = KNeighborsClassifier(n_neighbors=3)
+
+# Train the model using the training data
+knn.fit(X_train, y_train)
+
+# Make predictions on the test data
+y_pred = knn.predict(X_test)
+
+# Calculate the accuracy of the model
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
